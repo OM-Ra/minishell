5a26972365dbd35296d0220a270a541f57aac1fa
OM-Ra <syndeft@gmail.com>
2020-05-27 21:54:11 +0300

    main

--- src/main.c
+++ src/main.c
@@ -17,21 +17,6 @@ static void	sh21_finish(char **sh_environ)
 	ft_strdel_split(sh_environ);
 }
 
-
-static char	*test_sh(void)
-{
-	char			buf[BUFSIZ];
-	int				i;
-
-	ft_putstr("$> ");
-	if ((i = read(STDIN_FILENO, buf, BUFSIZ)) > 1)
-	{
-		buf[i - 1] = '\0';
-		return (ft_strcpy(ft_strnew(i - 1), buf));
-	}
-	return (NULL);
-}
-
 int			main(int argc, char **argv, char **env)
 {
 	t_init			init;
@@ -42,8 +27,7 @@ int			main(int argc, char **argv, char **env)
 	sh21_init(&init, env, &init.execlist);
 	while (1)
 	{
-		// input_begin(&init.execlist, &init.inp);
-		init.inp.str_for_parse = test_sh();
+		input_begin(&init.execlist, &init.inp);
 		if ((!init.inp.str_for_parse && init.inp.key == ('d' & 0x1f)))
 			break ;
 		else if (init.inp.str_for_parse &&
