1a5fc50eb56c25f876799068a5bbb58ca1990359
OM-Ra <syndeft@gmail.com>
2020-05-30 13:53:56 +0300

    Добавлены функции в список.

--- src/main.c
+++ src/main.c
@@ -17,6 +17,20 @@ static void	sh21_finish(char **sh_environ)
 	ft_strdel_split(sh_environ);
 }
 
+static char	*test_sh(void)
+{
+	char			buf[BUFSIZ];
+	int				i;
+
+	ft_putstr("$> ");
+	if ((i = read(STDIN_FILENO, buf, BUFSIZ)) > 1)
+	{
+		buf[i - 1] = '\0';
+		return (ft_strcpy(ft_strnew(i - 1), buf));
+	}
+	return (NULL);
+}
+
 int			main(int argc, char **argv, char **env)
 {
 	t_init			init;
@@ -27,13 +41,16 @@ int			main(int argc, char **argv, char **env)
 	sh21_init(&init, env, &init.execlist);
 	while (1)
 	{
-		input_begin(&init.execlist, &init.inp);
+		// input_begin(&init.execlist, &init.inp);
+		init.inp.str_for_parse = test_sh();
 		if ((!init.inp.str_for_parse && init.inp.key == ('d' & 0x1f)))
 			break ;
 		else if (init.inp.str_for_parse &&
 			!parser(&init.execlist, &(init.inp.str_for_parse), &init.prs))
 			check_choice(&init.execlist, init.prs.beg);
 		free_befor_exec(&init.prs);
+		if (init.inp.str_for_parse)
+			free(init.inp.str_for_parse);
 	}
 	input_finish(&init.inp);
 	sh21_finish(init.execlist.sh_environ);
